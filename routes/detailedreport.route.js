ba1fc72f2367c23991970aae96803164afcd4fd4
tarun . <tarun120501@gmail.com>
2025-05-08 15:49:31 +0530

    Commit at 08May, 3.50pm

--- routes/detailedreport.route.js
+++ routes/detailedreport.route.js
@@ -4,7 +4,7 @@ const detailedReportController = require("../controllers/detailedreport.controll
 
 // DetailedReport endpoints
 router.post("/api/detailedreports", detailedReportController.createDetailedReport);
-router.post("/api/detailedreports/bulk", detailedReportController.bulkUploadDetailedReports); // Add this line
+router.post("/api/detailedreports/bulk", detailedReportController.bulkUploadDetailedReports); 
 router.get("/api/detailedreports", detailedReportController.getDetailedReports);
 router.get("/api/detailedreports/:id", detailedReportController.getDetailedReportById);
 router.get("/api/students/:regNumber/reports", detailedReportController.getDetailedReportsByStudentId);
